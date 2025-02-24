bed02abda033d2d948b89e90d0eef13fa63cdf14
Rahul Repale <workwithrahul14@gmail.com>
2025-02-24 10:55:13 +0530

    Solved countVowels.js

--- week-2/week-2-js/medium/countVowels.js
+++ week-2/week-2-js/medium/countVowels.js
@@ -7,6 +7,13 @@
 
 function countVowels(str) {
     // Your code here
+  let arr = str.toLowerCase().split('')
+    let count = arr.filter((e) => {
+      if(e == 'a' || e == 'e' || e == 'i' || e  == 'o' || e == 'u'){
+        return e 
+      }
+    })
+    return count.length
 }
 
 module.exports = countVowels;
